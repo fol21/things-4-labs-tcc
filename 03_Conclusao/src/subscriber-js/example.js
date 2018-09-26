9aeb6eddea19116ca226856d68268bcc8d10b210
Fernando <folivelima@gmail.com>
2018-09-26 16:58:02 -0300

    content refactor

--- 03_Conclusao/src/subscriber-js/example.js
+++ 03_Conclusao/src/subscriber-js/example.js
@@ -60,16 +60,3 @@ monitor.onMessage((topic, message) => {
 
 monitor.init();
 
-// const plotly = require('plotly')('fol21', 'y32TAWwBymoFOnT0vkvE');
-
-// var data = [
-//   {
-//     x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
-//     y: [1, 3, 6],
-//     type: "scatter"
-//   }
-// ];
-// var graphOptions = {filename: "date-axes", fileopt: "overwrite"};
-// plotly.plot(data, graphOptions, function (err, msg) {
-//     console.log(msg);
-// });
